cbe11bffbfbd9d6b87b123c82f1e0f1bafb7122d
Pinakure <pinakure@gmail.com>
2020-09-26 01:48:12 +0200

    wip

---
+++ PR2/ecosystem/world.hpp
@@ -0,0 +1,25 @@
+#ifndef CORE_WORLD
+#define CORE_WORLD
+
+#include "sector.hpp"
+
+class World {
+private:
+public:
+	static Sector *origin;
+	static int temperature;
+	static int cell_size;
+	static bool initialize();
+	static void deinitialize();
+	static void draw();
+	static void drawMiniMap();
+	static void drawSectorInfo();
+	static void update(double delta);
+	static void drawRadar(Sector *o, int recursivity = 0);
+	static void setOrigin(Sector *o);
+	static void shutdown();
+	static void transformCoordinates(int *px, int *py, float z);
+	
+};
+
+#endif
