73fb9c5adc187848565c0640b2c6f8be930ed2ba
Pinakure <pinakure@gmail.com>
2020-10-19 23:54:14 +0200

    Node compiler wip

--- dialogeditor/include/node.compile.js
+++ dialogeditor/include/node.compile.js
@@ -2,81 +2,100 @@ Node.prototype.compileText = function(indent=0){
     var ret = '';
     for(ai in this.attributes.lines.value){
         var at = this.attributes.lines.value[ai];
-        ret += getIndent(indent)+`typewriter.enqueue('${at}')\n`
+        ret += getIndent(indent)+`typewriter.enqueue('${at}')\n`;
     }
     return ret;
 }
 
 Node.prototype.compileChoice = function(indent=0){
-    var ret = '';
-    for(ai in this.attributes.lines.value){
-        var at = this.attributes.lines.value[ai];
-    //    ret += getIndent(indent)+`typewriter.enqueue('${at}')\n`
+    var ret = getIndent(indent)+`typewriter.clearchoice()\n`;
+    var aft = '';
+    for(ai in this.attributes.choices.value){
+        var t = this.attributes.choices.value[ai];
+        var value = t.value;
+        var node  = (t.node && t.node!='null') ? Editor.nodes[t.node] : null;
+        if(node){
+            ret+= getIndent(indent)+`typewriter.addchoice('${value}', '${node.id}')\n`;
+            aft+= getIndent(indent)+`if choice == '${node.id}':\n${ node.compile(indent+1)}\n`;
+        }
     }
-    return ret;
+    return ret+aft;
 }
 
 Node.prototype.compileWait = function(indent=0){
-    var ret = '';
-    for(ai in this.attributes.lines.value){
+    var ret = getIndent(indent)+'#wait\n';
+    /*for(ai in this.attributes.lines.value){
         var at = this.attributes.lines.value[ai];
     //    ret += getIndent(indent)+`typewriter.enqueue('${at}')\n`
-    }
+    }*/
     return ret;
 }
 
 
 Node.prototype.compilePicture = function(indent=0){
-    var ret = '';
-    for(ai in this.attributes.lines.value){
+    var ret = getIndent(indent)+'#picture\n';
+    /*for(ai in this.attributes.lines.value){
         var at = this.attributes.lines.value[ai];
     //    ret += getIndent(indent)+`typewriter.enqueue('${at}')\n`
-    }
+    }*/
     return ret;
 }
 
 Node.prototype.compileRumble = function(indent=0){
-    var ret = '';
-    for(ai in this.attributes.lines.value){
+    var ret = getIndent(indent)+'#rumble\n';
+    /*for(ai in this.attributes.lines.value){
         var at = this.attributes.lines.value[ai];
     //    ret += getIndent(indent)+`typewriter.enqueue('${at}')\n`
-    }
+    }*/
     return ret;
 }
 
 Node.prototype.compileFont = function(indent=0){
-    var ret = '';
-    for(ai in this.attributes.lines.value){
+    var ret = getIndent(indent)+'#font\n';
+    /*for(ai in this.attributes.lines.value){
         var at = this.attributes.lines.value[ai];
     //    ret += getIndent(indent)+`typewriter.enqueue('${at}')\n`
-    }
+    }*/
     return ret;
 }
 
 Node.prototype.compileStyle = function(indent=0){
-    var ret = '';
-    for(ai in this.attributes.lines.value){
+    var ret = getIndent(indent)+'#style\n';
+    /*for(ai in this.attributes.lines.value){
         var at = this.attributes.lines.value[ai];
     //    ret += getIndent(indent)+`typewriter.enqueue('${at}')\n`
-    }
+    }*/
     return ret;
 }
 
 
+Node.prototype.compileChildren = function(indent=0){
+    var code = '';
+    for(ci in this.children){
+        var child = this.children[ci];
+        code += child.compile(indent);
+    }
+    return code;
+}
+
 Node.prototype.compile = function(indent=0){
     var code = '';
     switch(this.type){
-        case Types.TEXT:    code += this.compileText(indent); break;
-        case Types.CHOICE:  code += this.compileChoice(indent); break;
-        case Types.WAIT:    code += this.compileWait(indent); break;
+        /* BRANCHING NODES - DO NOT compile children! */
+
+        //case Types.BRANCH: return this.compileBranch(indent);
+        case Types.CHOICE:  return this.compileChoice(indent);  
+        
+        /* NON-BRANCHING NODES - append children code or script will end there */
+
+        case Types.FONT:    code += this.compileFont(indent);    break;
         case Types.PICTURE: code += this.compilePicture(indent); break;
-        case Types.RUMBLE:  code += this.compileRumble(indent); break;
-        case Types.FONT:    code += this.compileFont(indent); break;
-        case Types.STYLE:   code += this.compileStyle(indent); break;
+        case Types.RUMBLE:  code += this.compileRumble(indent);  break;
+        //case Types.SCRIPT:  code += this.compileScript(indent);  break;
+        case Types.TEXT:    code += this.compileText(indent);    break; 
+        case Types.STYLE:   code += this.compileStyle(indent);   break;
+        case Types.WAIT:    code += this.compileWait(indent);    break;
     }
-    for(ci in this.children){
-        var child = this.children[ci];
-        code += child.compile();
-    }
+    code += this.compileChildren(indent);
     return code;
 }
