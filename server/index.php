7501b862cf6e7c61d3709ac66b492da0ae08769b
al <aperez@arnoia.com>
2021-06-28 02:49:01 +0200

    Server presentation

---
+++ server/index.php
@@ -0,0 +1,9 @@
+<html style="height: 100%;width: 100%;">
+<head>
+<title>BlackBox Server #1</title>
+</head>
+<body style="width:100%; text-align: center;height: 100%; vertical-align: middle; line-height: 600px;"> 
+ <!-- <?php phpinfo() ?> -->
+ONLINE
+</body>
+</html>
